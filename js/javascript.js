e1f18f77a6190637a6e2d6e60c56963cbf2ec27a
Chl0eK3nt <45670198+Chl0eK3nt@users.noreply.github.com>
2020-07-24 16:29:58 -0400

    yeet

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 // MAP
 
-const map = L.map("map").setView([35.9027173,-79.0500057], 10);
+const map = L.map("map").setView([35.904597,-79.0460367], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -8,9 +8,8 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 
-//TILE LAYERS
+// GeoREF TILE
 
-// Add historical layer
 var GeoRef = L.tileLayer('./historicaltiles/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="http://osm.org/copyright">Historical</a> contributors'
 }).addTo(map);
@@ -19,6 +18,18 @@ attribution: '&copy; <a href="http://osm.org/copyright">Historical</a> contribut
 
 // MARKERS
 
+    var morrison = L.icon({
+        iconUrl: 'https://si.wsj.net/public/resources/images/PJ-AX628_build1_F_20101025183027.jpg',
+        iconSize: [50, 50], // size of the icon
+        popupAnchor: [0,-15]
+        });
+    var customPopup = "Morrison Dorm, My Home Next Semester<br/><img src='https://i.pinimg.com/originals/ed/7e/8d/ed7e8d53ba04fcc557ed0cf477d76a08.jpg' alt='go heels' width='250px'/>";
+    var customOptions =
+        {
+        'maxWidth': '500',
+        'className' : 'custom'
+        }
+    L.marker([35.904597,-79.0460367], {icon: morrison}).bindPopup(customPopup,customOptions).addTo(map);
 
 
 // GET latlng onClick
